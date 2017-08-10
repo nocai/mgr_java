3444285636c7e4e02ce61942037a7597b8b2967e
liujun <395822362@qq.com>
2017-08-10 20:36:26 +0800

    init

---
+++ src/main/java/com/mgr/MgrJavaApplication.java
@@ -0,0 +1,12 @@
+package com.mgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgrJavaApplication.class, args);
+	}
+}
